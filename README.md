0e7b3358ec1e16711efd43023b4960be930d7d24
ramana <ramanamurthy.bure@gmail.com>
2022-02-24 21:01:41 +0530

    -> Added content to the Readmed file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SeleniumCore
+
+This project is mainly to have all the practical examples of the Selenium core methods
+We need to add below jar's manually to this project.
+1. TestNG Library
+2. Apache commons IO
+
--- src/practice/selenium4/features/HightAndWidthOfElement.java
+++ src/practice/selenium4/features/HightAndWidthOfElement.java
@@ -1,14 +1,10 @@
 package practice.selenium4.features;
-import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
